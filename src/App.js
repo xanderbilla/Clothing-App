393af70edbfce57cbcc746d39a60ee7f744a1ee4
VIKAS SINGH <xanderbilla@gmail.com>
2023-04-27 02:53:20 +0530

    B1# Added Compoennt for Routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import ProductPage from './pages/ProductPage'
+import Cart from './pages/Cart'
+import Error from './pages/Error'
+
 import Home from './pages/Home'
 import {Route, Routes } from 'react-router-dom'
 
