15ab9609d409eb4cde78ac10bc8175c3de89520d
VIKAS SINGH <xanderbilla@gmail.com>
2023-04-29 18:47:40 +0530

    U6# Updates the static data

--- src/App.js
+++ src/App.js
@@ -6,21 +6,22 @@ import ProductList from './pages/ProductList'
 import Cart from './pages/Cart'
 import Error from './pages/Error'
 import ProfilePage from './pages/ProfilePage'
-
+import { popularProducts } from './static/data'
 import Home from './pages/Home'
 import {Route, Routes } from 'react-router-dom'
 import SignUp from './pages/SignUp'
 import Login from './pages/Login'
 
 const App = () => {
+
 const [isOpen, setIsOpen] = useState(false)
   return (
     <>
       <Navbar isOpen={isOpen} setIsOpen={setIsOpen}/>
         <Routes>
-          <Route path='/' element={ <Home/>} />
+          <Route path='/' element={ <Home popularProducts={popularProducts}/>} />
           <Route path='/product/:id' element={<ProductPage/> } />
-          <Route path='/category/:id' element={<ProductList/> } />
+        <Route path='/category/:id' element={<ProductList popularProducts={popularProducts} /> } />
           <Route path='/cart' element={<Cart/> } />
           <Route path='/profile' element={<ProfilePage/> } />
           <Route path='/register' element={<SignUp/> } />
