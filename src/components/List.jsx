4b6a0c2dc12ed56a43c869ec524425adeec63d7e
VIKAS SINGH <xanderbilla@gmail.com>
2023-05-02 08:48:07 +0530

    U2# Added backend

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,19 +1,53 @@
-import React, { useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Product from './Product'
 import styles from '../styles/list.module.css'
+import { API } from 'aws-amplify'
 
-const List = ({ cat, filters, sort, popularProducts }) => {
-    // const [products, setProducts] = useState([])
-    // const [filteredProducts, setFilteredProducts] = useState([])
+const List = ({ cat, filters, sort }) => {
+  const [products, setProducts] = useState([])
+  const [filteredProducts, setFilteredProducts] = useState([])
 
-    useEffect(() => {
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const apiName = 'apic2a5b53d';
+        API.get(apiName, `/products?category=${cat}`).then((response) => {
+          setProducts(response)
+        }).catch((error) => {
+          console.log(error);
+        });
+      } catch (error) { }
+    };
+    getProducts()
+  }, [cat])
 
-    }, [cat])
+  useEffect(() => {
+    cat && setFilteredProducts(
+      products.filter(item => Object.entries(filters).every(([key, value]) => item[key].includes(value)))
+    )
+  }, [products, cat, filters])
+
+  useEffect(() => {
+    if (sort === "newest") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.createdAt - b.createdAt)
+      )
+    } else if ((sort === "asc")) {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.discount_price - b.discount_price))
+    }
+    else {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => b.discount_price - a.discount_price))
+    }
+  }, [sort])
 
   return (
-      <div className={styles.list}>{popularProducts?.map(item => (
-          <Product item={item} key={ item._id}/>
-    ))}</div>
+    <div className={styles.list}>
+      {cat
+        ? filteredProducts.map((item) => <Product item={item} key={item.prodId} />)
+        : products.slice(0, 4).map((item) => <Product item={item} key={item.prodId} />)}
+    </div>
   )
 }
 
