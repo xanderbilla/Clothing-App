050754796d4d5c819e369e2cccd2a4467376bccb
VIKAS SINGH <xanderbilla@gmail.com>
2023-05-02 08:49:22 +0530

    U1# Integerated with API

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom';
 
 const Product = ({ item }) => {
   return (
-    <Link className={styles.link} to={`/product/${item._id}`}>
+    <Link className={styles.link} to={`/product/${item.prodId}`}>
       <div className={styles.card}>
         <div className={styles.image}>
           {item.isNew && <span className={styles.card_info}>New Season</span>}
-          <img src={item.img} alt="" className={styles.main_img} />
-          <img src={item.img2} alt="" className={styles.second_img} />
+          <img src={item.img[0]} alt="" className={styles.main_img} />
+          <img src={item.img[1]} alt="" className={styles.second_img} />
         </div>
         <h2 className={styles.card_title}>{item.title}</h2>
         <div className={styles.card_prices}>
-          <h3 className={styles.card_price}>${item.oldPrice}</h3>
-          <h3 className={styles.card_price}>${item.price}</h3>
+          <h3 className={styles.card_price}>${item.original_price}</h3>
+          <h3 className={styles.card_price}>${item.discount_price}</h3>
         </div>
       </div>
     </Link>
