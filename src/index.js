a6f95f7b9d6e84d962e24f1159a8f2b6692cffee
VIKAS SINGH <xanderbilla@gmail.com>
2023-05-03 03:11:11 +0530

    U2# Assigned Provider for Redux Toolkit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { Amplify } from 'aws-amplify';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import store from './redux/store';
 
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
@@ -11,11 +13,11 @@ Amplify.configure(awsExports);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
