8298b513241c67fc9af756023250788c7f17c68e
VIKAS SINGH <xanderbilla@gmail.com>
2023-05-03 01:13:07 +0530

    U1# Added Amplify Configuration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Amplify } from 'aws-amplify';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
