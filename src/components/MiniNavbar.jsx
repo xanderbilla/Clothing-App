cf75a873a3692e5c4b6defa80921c7054329567f
VIKAS SINGH <xanderbilla@gmail.com>
2023-05-08 04:34:40 +0530

    U1# Added Routes to select option

--- src/components/MiniNavbar.jsx
+++ src/components/MiniNavbar.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/miniNavbar.module.css'
 import LaunchIcon from '@mui/icons-material/Launch';
+import { useNavigate } from 'react-router-dom';
 
 const MiniNavbar = () => {
+    const [value, setValue] = useState('All')
+    const redirect = useNavigate()
+
+    const handleSelect = (event) => {
+        const selectedValue = event.target.value;
+        setValue(selectedValue);
+        redirect(`/category/${selectedValue}`);
+    }
+
+    // Set the "value" attribute to the value of the state variable "value"
+    // Set the "defaultValue" attribute to "All" (the default option)
     return (
         <div className={styles.mininavbar}>
             <div className={styles.left}>
-                <select className={styles.all_categories} name="categories" title="categories" defaultValue="All">
+                <select className={styles.all_categories} name="categories" title="categories" value={value} defaultValue="All" onChange={handleSelect}>
                     <option className={styles.categories__selectOption} disabled value="All">All Categories</option>
                     <option className={styles.categories__selectOption} value="Men">Men</option>
                     <option className={styles.categories__selectOption} value="Women">Women</option>
