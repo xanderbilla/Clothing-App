092c9c172d537060436c1c70bd081ac1cd407be8
VIKAS SINGH <xanderbilla@gmail.com>
2023-05-22 19:08:23 +0530

    U2# Implemented API

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
-import styles from '../styles/orders.module.css'
+import React, { useEffect, useState } from 'react';
+import styles from '../styles/orders.module.css';
 import OrderCard from '../components/OrderCard';
+import { API } from 'aws-amplify';
+import { Link } from 'react-router-dom';
 
 const Orders = () => {
-    return (
-        <div className={styles.container}>
-            <div className={styles.top}>
-            <h1 className={styles.title}>YOUR ORDERS</h1>
-                <button className={styles.button}>Continue Shopping</button>
-        </div>
-            <div className={styles.orders}>
-                <OrderCard />
-            </div>
-        </div>
-    )
-}
+  const [orders, setOrders] = useState([]);
 
-export default Orders
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
+  const fetchOrders = async () => {
+    try {
+      const response = await API.get('ecommerceApiOrders', '/orders');
+      setOrders(response);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+console.log(orders);
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <h1 className={styles.title}>YOUR ORDERS</h1>
+              <Link to='/'>
+                  <button className={styles.button}>Continue Shopping</button>
+                  </Link>
+      </div>
+      <div className={styles.orders}>
+        {orders && orders.map((order, i) => (
+          <OrderCard cart={order} key={i} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
